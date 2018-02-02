6ba722f84b79d343d061644bdd09bb229d7bcc93
github username <github email address>
2018-02-02 14:04:59 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -4,33 +4,30 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  @@songs = []
+
+
+  @@all = []
+  extend Memorable
+  extend Findable
+  include Paramable
 
   def initialize
-    @@songs << self
+    super
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@all.detect{|a| a.name == name}
+  # end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def self.reset_all
-    @@songs.clear
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-
-  def self.count
-    self.all.count
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 
   def self.all
-    @@songs
+    @@all
   end
 end
